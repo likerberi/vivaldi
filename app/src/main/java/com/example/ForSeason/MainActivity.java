9f1d9b2b177dcbaaaf486bd72c204dbb37a2b87b
stepbystep <likerberi97@gmail.com>
2019-03-27 22:36:56 +0900

    Firebase frame init

--- app/src/main/java/com/example/ForSeason/MainActivity.java
+++ app/src/main/java/com/example/ForSeason/MainActivity.java
@@ -7,14 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView loginTextButton;
+    private TextView loginTextButton, signInButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+          mAuth = FirebaseAuth.getInstance(); // Login-chk
 
         loginTextButton = findViewById(R.id.loginButton);
         loginTextButton.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                Intent goIntent = new Intent(MainActivity.this, CheckActivity.class);
+                startActivity(goIntent);
+                finish();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Intent goIntent = new Intent(MainActivity.this, OverallActivity.class);
+        startActivity(goIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/ForSeason/TestActivity.java
+++ app/src/main/java/com/example/ForSeason/TestActivity.java
@@ -1,15 +1,60 @@
 package com.example.ForSeason;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class TestActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
+        String email = "";
+        String password = "";
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                        }
+
+                        // ...
+                    }
+                });
+
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Intent goIntent = new Intent(TestActivity.this, OverallActivity.class);
+        startActivity(goIntent);
+        finish();
+    }
+
 }
+
+
