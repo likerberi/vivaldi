04c9ded24063dd54e65476af8012e2a64a37fc90
S.Y.C <likerberi97@gmail.com>
2023-02-08 21:58:36 +0900

    7,8

---
+++ Prototype/src/UnderlinePen.java
@@ -0,0 +1,13 @@
+import framework.Product;
+
+public class UnderlinePen implements Product {
+    @Override
+    public void use(String s) {
+
+    }
+
+    @Override
+    public Product createCopy() {
+        return null;
+    }
+}
