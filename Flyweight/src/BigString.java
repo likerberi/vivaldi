db1af83c86c33d1d103e20e57618fcc00659b316
S.Y.C <likerberi97@gmail.com>
2023-05-16 23:33:27 +0900

    Flyweight

---
+++ Flyweight/src/BigString.java
@@ -0,0 +1,16 @@
+public class BigString {
+    private BigChar[] bigChars;
+    public BigString(String string) {
+        BigCharFactory factory = BigCharFactory.getInstance();
+        bigChars = new BigChar[string.length()];
+        for (int i = 0; i<bigChars.length; i++ ) {
+            bigChars[i] = factory.getBigChar(string.charAt(i));
+        }
+    }
+
+    public void print() {
+        for (BigChar bc:bigChars) {
+            bc.print();
+        }
+    }
+}
