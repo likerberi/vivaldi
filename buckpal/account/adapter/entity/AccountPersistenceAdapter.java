c8301ee8e15bab3c567c79c191cce20eb860c23d
S.Y.C <likerberi97@gmail.com>
2023-07-11 23:37:46 +0900

    update5

--- buckpal/account/adapter/entity/AccountPersistenceAdapter.java
+++ buckpal/account/adapter/entity/AccountPersistenceAdapter.java
@@ -5,6 +5,26 @@ class AccountPersistenceAdapter implements LoadAccountPort, UpdateAccountStatePo
     private final ActivityRepository activityRepository;
     private final AccountMapper accountMapper;
 
-    public Account loadAccount(){}
+    public Account loadAccount(
+            AccountId accountId,
+            LocalDateTime baselineDate
+    ){
+        AccountJpaEntity account = accountRepository.findById(accountId.getValue())
+                .orElseThrow();
+        List<ActivityJpaEntity> activities =
+                activityRepository.findByOwnerSince(
+                        accountId.getValue(), baselineDate
+                );
+        Long withdrawalBalance = orZero();
 
+        Long depositBalance = orZero();
+        return accountMapper.mapToDomainEntity(
+                account, activities,
+                withdrawalBalance, depositBalance
+        );
+    }
+
+    private Long orZero(Long value) {
+        return value == null ? 0L : value;
+    }
 }
